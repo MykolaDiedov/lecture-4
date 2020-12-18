125c326eabaf8db9b2501cc3a2c3b0a3eb69cbdc
MykolaDiedov <diedov.nn@gmail.com>
2020-12-18 16:58:42 +0200

    js change

--- assets/main.js
+++ assets/main.js
@@ -6,9 +6,9 @@ var index = 0;
 var out ="";
 while ( index <= height ) {
     for ( var j = 0; j < height - index; j++ ) {
-        out += "0";}
+        out += " ";}
     for ( var j = 0; j < 2 * index + 1; j++ ) {
-        out += "8";}
+        out += "*";}
     index++;
     out += "\n";}
 alert(out);
